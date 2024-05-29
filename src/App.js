6144a9e326a462d2f5f00666c4f95614c4f9748e
duridev <duridev@outlook.com>
2024-05-29 12:26:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO WORLD!!
         </p>
         <a
           className="App-link"
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,5 @@
+function Header(){
+    return <img src="/img/header.png" alt="Org-header" />
+}
+
+export default Header
