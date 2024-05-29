3f762c2c6c0a689a007a4fd8c4abbf6e8282093a
duridev <duridev@outlook.com>
2024-05-29 12:40:55 -0400

    creacion componente Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,9 @@
+import "./Header.css"
+
 function Header(){
-    return <img src="/img/header.png" alt="Org-header" />
+    return <header className="header">
+        <img src="/img/header.png" alt="Org-header" />
+        </header>
 }
 
 export default Header
